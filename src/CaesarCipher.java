8c77f73da8383c9cb1cd95d8707a0dbf7b8379e5
JonatanV <jonatan.vannberg@elev.ga.ntig.se>
2020-02-05 07:40:35 +0100

    Nu kan båda programmen göra hundra random strings och kryptera de efter varandra

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,51 +1,70 @@
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 //A Java Program to illustrate Caesar Cipher Technique
-class CaesarCipher
-{
+class CaesarCipher {
     // Encrypts text using a shift od s
-    public static StringBuffer encrypt(String text, int s)
-    {
-        StringBuffer result= new StringBuffer();
-        for (int i=0; i<text.length(); i++)
-        {
-            if (Character.isUpperCase(text.charAt(i)))
-            {
-                char ch = (char)(((int)text.charAt(i) +
+    public static StringBuffer encrypt(String text, int s) {
+
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) {
+                char ch = (char) (((int) text.charAt(i) +
                         s - 65) % 26 + 65);
                 result.append(ch);
-            }
-            else
-            {
-                char ch = (char)(((int)text.charAt(i) +
+            } else {
+                char ch = (char) (((int) text.charAt(i) +
                         s - 97) % 26 + 97);
                 result.append(ch);
             }
         }
         return result;
     }
-    // Driver code
-    public static void main(String[] args) throws InterruptedException {
 
-        Scanner tgb = new Scanner(System.in);
 
-        System.out.println("Skriv din klartext BARA STORA BOKSTÄVER");
-        String text = tgb.nextLine();
-        System.out.println("Välj shift");
+        // Driver code
+    public static void main(String[] args){
+        int s = 4;
+        boolean x = true;
+            for (int n = 8; n <=100 ; n++) {
+                String input = RandomString.getAlphaNumericString(n);
+                System.out.println("Text:  " + input);
+                System.out.println("Shift : " + s);
+                long starttime = System.nanoTime();
+                System.out.println("Cipher: " + encrypt(input, s));
+                long timepassed = System.nanoTime() - starttime;
+                System.out.println("Input length: "+input.length());
+                System.out.println("Encryption finished in " + timepassed + " Nanoseconds"+"\n");
+            }
+       }
+    public static class RandomString {
+        public static Random r;
+        int n;
 
-        int s = tgb.nextInt();
-            boolean x = true;
+        // function to generate a random string of length n
+        public static String getAlphaNumericString(int n) {
+            r = new Random();
+            // chose a Character random from this String
+            String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                    "abcdefghijklmnopqrstuvxyz";
 
-        System.out.println("Text  : " + text);
-        System.out.println("Shift : " + s);
-        long starttime = System.currentTimeMillis();
-        System.out.println("Cipher: " + encrypt(text, s));
-        long timepassed=System.currentTimeMillis()-starttime;
-        System.out.println("Encryption finished in " + timepassed + " Milliseconds");
+            // create StringBuffer size of AlphaNumericString
+            StringBuilder sb = new StringBuilder(n);
 
+            for (int i = 0; i < n; i++) {
+
+                // generate a random number between
+                // 0 to AlphaNumericString variable length
+                int index = (int) (AlphaNumericString.length() * r.nextDouble());
+
+                // add Character one by one in end of sb
+                sb.append(AlphaNumericString
+                        .charAt(index));
+            }
+
+            return sb.toString();
+        }
 
 
     }
 }
+
--- src/testTrans.java
+++ src/testTrans.java
@@ -1,66 +1,83 @@
 import java.util.*;
-class testTrans
-{
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the plain text");
-        String pl=sc.nextLine();
-        String demo="";
-        String s="";
-        int start=0;
-        for(int i=0;i<pl.length();i++)
-        {
-            if(pl.charAt(i)==' ')
-            {
-                s=s+pl.substring(start,i);
-                start=i+1;
+
+class testTrans {
+    public static void main(String args[]) {
+        String pl;
+        for (int i = 0; i <= 100; i++) {
+            pl = RandomString.getAlphaNumericString(i);
+
+
+        String demo = "";
+        String s = "";
+        int start = 0;
+
+        for (int q = 0; i < pl.length(); q++) {
+            if (pl.charAt(i) == ' ') {
+                s = s + pl.substring(start, q);
+                start = q + 1;
             }
         }
-        s=s+pl.substring(start);
 
+        s = s + pl.substring(start);
 
-
-
-        int k=s.length();
-        int l=0;
-        int col=3;
-        int row=s.length()/col;
-        char ch[][]=new char[row][col];
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                if(l<k)
-                {
-                    ch[i][j]=s.charAt(l);
+        int k = s.length();
+        int l = 0;
+        int col = 4;
+        int row = s.length() / col;
+        char ch[][] = new char[row][col];
+        long starttime = System.nanoTime();
+        for (int w = 0; w < row; w++) {
+            for (int j = 0; j < col; j++) {
+                if (l < k) {
+                    ch[w][j] = s.charAt(l);
                     l++;
-                }
-                else
-                {
-                    ch[i][j]='#';
+                } else {
+                    ch[w][j] = '#';
                 }
             }
         }
 
-
-        char trans[][]=new char[col][row];
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                trans[j][i]=ch[i][j];
+        char trans[][] = new char[col][row];
+        for (int e = 0; e < row; e++) {
+            for (int j = 0; j < col; j++) {
+                trans[j][e] = ch[e][j];
             }
         }
 
-        for(int i=0;i<col;i++)
-        {
-            for(int j=0;j<row;j++)
-            {
-                System.out.print(trans[i][j]);
+        for (int t = 0; t < col; t++) {
+            for (int y = 0; y < row; y++) {
+                System.out.print(trans[t][y]);
             }
         }
+        long timepassed = System.nanoTime() - starttime;
+        System.out.println("\n" + timepassed + " Nanoseconds passed");
+    }
+}
+    public static class RandomString {
+        public static Random r;
+        int n;
 
-        System.out.println();
+        // function to generate a random string of length n
+        public static String getAlphaNumericString(int n) {
+            r = new Random();
+            // chose a Character random from this String
+            String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                    "abcdefghijklmnopqrstuvxyz";
+
+            // create StringBuffer size of AlphaNumericString
+            StringBuilder sb = new StringBuilder(n);
+
+            for (int i = 0; i < n; i++) {
+
+                // generate a random number between
+                // 0 to AlphaNumericString variable length
+                int index = (int) (AlphaNumericString.length() * r.nextDouble());
+
+                // add Character one by one in end of sb
+                sb.append(AlphaNumericString
+                        .charAt(index));
+            }
+            return sb.toString();
+        }
     }
 }
